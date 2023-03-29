d901317ed0f5069763605a58877106770a6ef557
kingwzun <1905952141@qq.com>
2023-03-29 16:34:15 +0800

    实现Chronometer计时器（点击登陆后进入界面）

--- app/src/main/java/cn/edu/sdut/myapplication/LoginActivity.java
+++ app/src/main/java/cn/edu/sdut/myapplication/LoginActivity.java
@@ -4,9 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
+import android.widget.ToggleButton;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -15,14 +24,68 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.constraint_login);
         Button btnLogin=findViewById(R.id.btnLogin);
+        EditText edit_xm=findViewById(R.id.edit_xm);
+        EditText edit_mm = findViewById(R.id.edit_mm);
+        ToggleButton toggle= findViewById(R.id.TgBtm);
+        RadioButton rb_js= findViewById(R.id.rb_js);
+        RadioButton rb_xs= findViewById(R.id.rb_xs);
+        RadioGroup radioGroup=findViewById(R.id.usertype);
+        CheckBox checkbox=findViewById(R.id.checkbox);
+
+        checkbox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    Toast.makeText(LoginActivity.this,"记住密码", Toast.LENGTH_LONG);
+                }
+            }
+        });
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String userType="学生";
+//                ----------方法1：使用RadioButton-------------
+//                if(rb_js.isChecked()){
+//                    userType="教师";
+//                }
+//                -------方法2:使用RadioGroup----------------
+                int rb_id=radioGroup.getCheckedRadioButtonId();
+                if(rb_id==rb_js.getId()){
+                    userType="教师";
+                }
                 Intent intent = new Intent(LoginActivity.this,MainActivity.class);
                 startActivity(intent);
 //               关闭登录界面（点击返回按键不会再回到登陆界面）
 //                LoginActivity.this.finish();
             }
         });
+
+        edit_xm.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                Log.d("login","onTextChanged:"+s);//打印日志
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {//Editable是文本框输入的内容的数据类型    这是 s=edit_xm.getText()
+                Log.d("login","afterTextChanged:"+s.toString());//打印日志
+            }
+        });
+        toggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+
+                    // 选中状态时需要执行的代码
+                } else {
+                    //未选中状态时需要执行的代码
+                }
+            }
+        });
+
     }
 }
--- app/src/main/java/cn/edu/sdut/myapplication/MainActivity.java
+++ app/src/main/java/cn/edu/sdut/myapplication/MainActivity.java
@@ -3,14 +3,41 @@ package cn.edu.sdut.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Chronometer;
 
 public class MainActivity extends AppCompatActivity {
-
+    Chronometer jishiqi;
+    Button btnSendSms;
+    int shijian=10;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.login_layout);//R.类.名称
+        btnSendSms =findViewById(R.id.btnDome);
+        jishiqi=findViewById(R.id.jishiqi);
+        jishiqi.setOnChronometerTickListener(new Chronometer.OnChronometerTickListener() {
+            @Override
+            public void onChronometerTick(Chronometer chronometer) {
+                shijian--;
+                if(shijian>0){
+                    btnSendSms.setText(shijian+"秒后重新发送");
+                }else{
+                    btnSendSms.setEnabled(true);
+                    btnSendSms.setText(R.string.btnSendSmsText);
+                }
+            }
+        });
+        btnSendSms.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                shijian=10;
+                jishiqi.start();
+                btnSendSms.setEnabled(false);
+            }
+        });
         //R是Android内部的类，自动的掌管Android的资源
         // （.的实质是static类）
         //.名称  是系统会自动增加一个int类型的常量
