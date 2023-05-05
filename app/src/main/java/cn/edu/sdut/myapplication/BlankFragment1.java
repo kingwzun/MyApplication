cb33d60a3f66a7d90e4dfbdd2dfb275c26452a63
kingwzun <1905952141@qq.com>
2023-05-05 17:05:13 +0800

    study Fragment

---
+++ app/src/main/java/cn/edu/sdut/myapplication/BlankFragment1.java
@@ -0,0 +1,29 @@
+package cn.edu.sdut.myapplication;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link BlankFragment1#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class BlankFragment1 extends Fragment {
+
+    public BlankFragment1() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_blank1, container, false);
+    }
+}
