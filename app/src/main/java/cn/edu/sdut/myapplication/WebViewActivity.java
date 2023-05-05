b29f08dc4772494d2b726b52496e4f7bdd5b11ca
kingwzun <1905952141@qq.com>
2023-05-05 21:55:16 +0800

    repair webView

--- app/src/main/java/cn/edu/sdut/myapplication/WebViewActivity.java
+++ app/src/main/java/cn/edu/sdut/myapplication/WebViewActivity.java
@@ -1,6 +1,9 @@
 package cn.edu.sdut.myapplication;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
@@ -8,22 +11,22 @@ import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.ProgressBar;
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-
 public class WebViewActivity extends AppCompatActivity {
     WebView webView;
     ProgressBar progressBar;
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web_view);
         webView=findViewById(R.id.web_view);
         progressBar=findViewById(R.id.progress_horizontal);
 
+        webView.setWebViewClient(new WebViewClient());//不用内置浏览器，而是继续使用WebView视图显示超链接给出的Web页
+        //让WebView视图支持JavaScript脚本，
         WebSettings settings=webView.getSettings();
         settings.setJavaScriptEnabled(true);
         progressBar.setMax(100);
+
         webView.setWebChromeClient(new WebChromeClient(){
             @Override
             public void onProgressChanged(WebView view, int newProgress) {
@@ -38,5 +41,16 @@ public class WebViewActivity extends AppCompatActivity {
             }
         });
         webView.loadUrl("https://www.sdut.edu.cn");
+    }
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if(keyCode==KeyEvent.KEYCODE_BACK){
+            if(webView.canGoBack()){
+                webView.goBack();
+            }else {
+                WebViewActivity.this.finish();
+            }
+        }
+        return true;
     }
 }
